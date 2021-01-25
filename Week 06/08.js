e487fbe224abf55cfd03f5c4557cd3d2d7cc2b83
susieshen <susieshen@lexinfintech.com>
2021-01-25 15:04:34 +0800

    susei cm

--- Week 06/08.js
+++ Week 06/08.js
@@ -1,4 +1,23 @@
-let str = 'a中国';
-console.log(str.charAt[0]);
-console.log(str.charAt[1]);
-console.log(str.charAt[2]);
+
+/*
+1.JavaScript是Unicode编码的，length,charCodeAt，charAt都是针对Unicode的
+2.URI中的querystring必须按照UTF8的编码进行传输,但是只针对非英文字符进行编码
+*/
+var writeUTF2 = function (str) {
+    var back = [];
+    for (var i = 0; i < str.length; i++) {
+        let code = str.charCodeAt(i);
+        if (0x00 <= code && code <= 0x7f) {//英文字符encodeURI没有转义
+            back.push(code &= 0xff);
+        } else{
+            let temp = encodeURI(str.charAt(i)).split('%').slice(1).map(item => parseInt(item,16));
+            back = back.concat([...temp]);
+        }
+    }
+    return back;
+}
+
+
+console.log('writeUTF2',writeUTF2('ab'))//[ 97, 98 ]
+console.log('writeUTF2',writeUTF2('中国'))//[ 228, 184, 173, 229, 155, 189 ]
+
